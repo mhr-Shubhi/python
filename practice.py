3e2f92fa21f8722afc05891fa378287c9290a205
Shubhi <shubhisinghmahar@gmail.com>
2026-02-04 16:56:53 +0530

    initial commit

---
+++ practice.py
@@ -0,0 +1,14 @@
+while True:
+    user_input= input("What would you like to do? A)add b)view c)total d)category e)exit").lower()
+
+    if user_input == "add":
+        print()
+    elif user_input == "view":
+        print
+    elif user_input=="total":
+        print()
+    elif user_input=="category":
+        print()
+    elif user_input=="exit":
+        break     
+
